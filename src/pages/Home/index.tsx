05ae07ed9e3012f3a3e67eee4ce9ba4dba2ab92c
Augusto Marsola <augusto.marsola@live.com>
2022-10-05 23:02:29 -0300

    Finished the style of home banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,57 @@
-import CoffeeBanner from '../../assets/caffeBanner.png'
-import { Container, TitleContent } from './styles'
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import CoffeeBanner from '../../assets/coffeeBanner.png'
+import {
+  BackgroundContainer,
+  Container,
+  Icon,
+  LeftContent,
+  ListContent,
+  TitleContent,
+} from './styles'
 
 export function Home() {
   return (
-    <Container>
-      <div>
-        <TitleContent>
-          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-          <h2>
-            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-            hora
-          </h2>
-        </TitleContent>
+    <BackgroundContainer>
+      <Container>
+        <LeftContent>
+          <TitleContent>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <h2>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </h2>
+          </TitleContent>
+          <ListContent>
+            <li>
+              <Icon color={'orange'}>
+                <ShoppingCart weight="fill" />
+              </Icon>{' '}
+              Compra simples e segura
+            </li>
+            <li>
+              <Icon color={'dark'}>
+                <Package weight="fill" />
+              </Icon>{' '}
+              Embalagem mantém o café intacto
+            </li>
+            <li>
+              <Icon color={'yellow'}>
+                <Timer weight="fill" />
+              </Icon>{' '}
+              Entrega rápida e rastreada
+            </li>
+            <li>
+              <Icon color={'purple'}>
+                <Coffee weight="fill" />
+              </Icon>{' '}
+              O café chega fresquinho até você
+            </li>
+          </ListContent>
+        </LeftContent>
         <div>
-          <ul>
-            <li>Compra simples e segura</li>
-            <li>Embalagem mantém o café intacto</li>
-            <li>Entrega rápida e rastreada</li>
-            <li>O café chega fresquinho até você</li>
-          </ul>
+          <img src={CoffeeBanner} alt="Copo de café com grãos de café" />
         </div>
-      </div>
-      <div>
-        <img src={CoffeeBanner} alt="Copo de café com grãos de café" />
-      </div>
-    </Container>
+      </Container>
+    </BackgroundContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,7 +1,14 @@
 import { styled } from '../../styles'
+import BackgroundBlur from '../../assets/backgroundBlur.svg'
+
+export const BackgroundContainer = styled('div', {
+  backgroundImage: `url(${BackgroundBlur})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'contain',
+})
 
 export const Container = styled('section', {
-  padding: '2rem 0',
+  padding: '5.75rem 0',
   margin: '0 auto',
   maxWidth: '70rem',
   marginTop: '6.75rem',
@@ -9,14 +16,64 @@ export const Container = styled('section', {
   gap: '3.5rem',
 })
 
+export const LeftContent = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-around',
+})
+
 export const TitleContent = styled('div', {
   h1: {
+    color: '$titleColor',
     fontFamily: '$header',
     fontSize: '3rem',
     fontWeight: 800,
+    lineHeight: 1.3,
   },
   h2: {
+    color: '$subtitleColor',
     fontSize: '1.25rem',
+    lineHeight: 1.3,
     fontWeight: 400,
+    marginTop: '1rem',
+  },
+})
+
+export const ListContent = styled('ul', {
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  listStyle: 'none',
+
+  li: {
+    gap: '0.75rem',
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '1.25rem',
+  },
+})
+
+export const Icon = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '0.5rem',
+  color: '$white',
+  borderRadius: 99999,
+
+  variants: {
+    color: {
+      orange: {
+        backgroundColor: '$yellow900',
+      },
+      yellow: {
+        backgroundColor: '$yellow500',
+      },
+      dark: {
+        backgroundColor: '$textColor',
+      },
+      purple: {
+        backgroundColor: '$purple500',
+      },
+    },
   },
 })
