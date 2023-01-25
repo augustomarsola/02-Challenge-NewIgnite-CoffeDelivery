05cf29027bb979517e9f63a5dad41c2d43a748ec
Augusto Marsola <augusto.marsola@live.com>
2023-01-24 22:25:24 -0300

    Finished Toast Added Coffee component

--- src/components/Toast/ToastAddedCoffee/ToastAddedCoffee.styles.ts
+++ src/components/Toast/ToastAddedCoffee/ToastAddedCoffee.styles.ts
@@ -1,6 +1,35 @@
-import { styled } from '@/styles'
 import * as Toast from '@radix-ui/react-toast'
+import { styled } from '@/styles'
+import { ToastRoot as MainRoot } from '../ToastProvider/ToastProvider.styles'
 
-export const ToastRoot = styled(Toast.Root, {
-  background: 'blue',
+export const ToastRoot = styled(MainRoot, {
+  background: '$purple500',
+  color: '$white',
+})
+
+export const ContentWrapper = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: 16,
+})
+
+export const ToastTitle = styled(Toast.Title, {
+  fontFamily: '$header',
+  fontSize: '1.25rem',
+})
+
+export const ToastButton = styled('button', {
+  fontFamily: '$header',
+  backgroundColor: '$purple500',
+  color: '$white',
+  padding: '0.25rem 0.5rem',
+  borderRadius: 4,
+  border: '2px solid $white',
+  cursor: 'pointer',
+  transition: '.2s',
+
+  '&:hover': {
+    backgroundColor: '$white',
+    color: '$purple500',
+  },
 })
--- src/components/Toast/ToastAddedCoffee/ToastAddedCoffee.tsx
+++ src/components/Toast/ToastAddedCoffee/ToastAddedCoffee.tsx
@@ -1,5 +1,6 @@
 import * as Toast from '@radix-ui/react-toast'
-import { ToastRoot } from '../ToastProvider/ToastProvider.styles'
+import * as Styles from './ToastAddedCoffee.styles'
+import { ShoppingCart } from 'phosphor-react'
 
 interface ToastProps {
   open: boolean
@@ -8,11 +9,16 @@ interface ToastProps {
 
 export default function ToastAddedCoffee({ open, setOpen }: ToastProps) {
   return (
-    <ToastRoot open={open} onOpenChange={setOpen}>
-      <Toast.Title>Pedido adicionado ao carrinho</Toast.Title>
-      <Toast.Action asChild altText="Finalizar Compra">
-        <button>Finalizar Compra</button>
-      </Toast.Action>
-    </ToastRoot>
+    <Styles.ToastRoot open={open} onOpenChange={setOpen}>
+      <Styles.ContentWrapper>
+        <ShoppingCart weight="fill" size={28} />
+        <div>
+          <Styles.ToastTitle>Pedido adicionado ao carrinho</Styles.ToastTitle>
+          <Toast.Action asChild altText="Finalizar Compra">
+            <Styles.ToastButton>Finalizar Compra</Styles.ToastButton>
+          </Toast.Action>
+        </div>
+      </Styles.ContentWrapper>
+    </Styles.ToastRoot>
   )
 }
