43c1b37ed24f31b85543fefd530bcd567d172c87
Augusto Marsola <augusto.marsola@newfold.com>
2023-04-04 22:24:52 -0300

    Initiate toggle stats change

--- src/components/Checkout/CompleteOrder/CompleteOrder.tsx
+++ src/components/Checkout/CompleteOrder/CompleteOrder.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react'
 import { SmallTitle } from '@/components/SmallTitle'
 import * as Styles from './CompleteOrder.styles'
 import { DefaultCard } from '@/components/DefaultCard'
+import * as ToggleGroup from '@radix-ui/react-toggle-group'
 
 export default function CompleteOrder() {
+  const [value, setValue] = useState('left')
+
   return (
     <Styles.Wrapper>
       <SmallTitle>Complete seu pedido</SmallTitle>
@@ -44,6 +48,21 @@ export default function CompleteOrder() {
             </Styles.DeliveryDescription>
           </Styles.DeliveryText>
         </Styles.DeliveryWrapper>
+        <ToggleGroup.Root
+          type="single"
+          value={value}
+          onValueChange={(value: string) => {
+            if (value) setValue(value)
+          }}
+        >
+          <ToggleGroup.Item value="Cartão de Crédito">
+            Cartão de Crédito
+          </ToggleGroup.Item>
+          <ToggleGroup.Item value="Cartão de Débito">
+            Cartão de Débito
+          </ToggleGroup.Item>
+          <ToggleGroup.Item value="Dinheiro">Dinheiro</ToggleGroup.Item>
+        </ToggleGroup.Root>
       </DefaultCard>
     </Styles.Wrapper>
   )
